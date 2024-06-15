69ffaf5d8ef24d1f195559ded4ddd1105990c600
Crystal Lee <crystallee@Crystals-MacBook-Pro.local>
2024-06-15 17:50:58 -0400

    Included more details to comments and cleaned up code for readability.

--- btc1855-assign2.R
+++ btc1855-assign2.R
@@ -14,14 +14,19 @@
 
 answer <- readline(prompt = "Please enter a three digit positive number: ")
 
-# readline() function inputs characters as strings, must convert input to numeric
+# readline() function converts all input as strings, must convert to numeric
 answer <- as.numeric(answer)
 
-# if input is NA, the input was not numeric
+#' Check if input is NA
+#' If NA, the input was not a number
+#' Stop the code if input is not a number so that it doesn't 
+#' continue to the next checks
 if(is.na(answer) == T){
   stop("Entry is not a number.")
 } 
 
+#' If number passed last check, it will now check whether it is positive and three digits
+#' If it doesn't pass, the code will stop running and not continue to the next check
 if(answer < 0) {
   stop(paste(answer,"is not a positive number."))
 } else if(answer < 100 || answer > 999) {
@@ -30,16 +35,20 @@ if(answer < 0) {
   print("Entry is a valid number.")
 }
 
-# convert number into a string so that we can separate the digits
+#' If number passed last check, it will now check if it is an armstrong number
+#' Convert number into a string so that we can separate the digits
 arm_number <- as.character(answer)
-#' explain why it's integers
-#' split the number into its digits
+
+#' Split the digits using strsplit() and convert to integers so we can do sum()
 arm_digits <-  as.integer(strsplit(arm_number, "")[[1]])
-# cube the individal digits
+
+# Cube each individual digit
 arm_cubed <- arm_digits^3
+
+# Sum all the cubed digits together
 sum_digits <- sum(arm_cubed)
 
-# check if entry is an armstrong number
+# Check if entry is an armstrong number
 if(sum_digits != answer){
   print(paste(answer,"is not an armstrong number."))
 } else {
